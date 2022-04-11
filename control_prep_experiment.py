fcea53b909116f4a0764bb805a4f84e68081f80b
SMa2021 <80334667+SMa2021@users.noreply.github.com>
2022-04-10 23:01:46 -0400

    add comments and consolidate floating code

--- control_prep_experiment.py
+++ control_prep_experiment.py
@@ -1,3 +1,10 @@
+"""
+This program reads in the file at TREATMENT_FILE_PATH, which is expected to contain entries that have 'author', 'subreddit' information.
+It then analyzes the entries by counting the number of submissions in each subreddit by a given author, normalizing the count, computing the mean of each subreddit and then identifying the subreddit that deviates the most from the mean for each author.
+The output is store in OUTPUT_FILE_NAME. 
+A couple of methods for summary statistics are also included. (get_num_unique_column_in_df, get_median_num_unique_subreddit_per_user)
+"""
+
 import pandas as pd
 import time
 
@@ -18,8 +25,6 @@ submissions = pd.read_csv(TREATMENT_FILE_PATH)
 def count_by_author_and_subreddit(submissions):
     return submissions.groupby(['author', 'subreddit']).size().to_frame(name= 'count').reset_index()
 
-counts_author_subreddit_pair = count_by_author_and_subreddit(submissions)
-
 # return the number of unique items in the specified column of the dataframe
 def get_num_unique_column_in_df(df, column_name):
     return len(df[column_name].unique())
@@ -32,14 +37,9 @@ def get_median_num_unique_subreddit_per_user(submissions):
 def get_author_per_subreddit(counts_author_and_subreddit):
     return counts_author_subreddit_pair.pivot_table(index='author', columns='subreddit', values='count')
 
-# print(counts_author_per_subreddit.head(5))
-counts_author_per_subreddit = get_author_per_subreddit(counts_author_subreddit_pair)
-
 # cast counts to a sparse datatype
 def get_sparse_df(df):
     return df.fillna(0).astype(pd.SparseDtype("int16"))
-count_array = get_sparse_df(counts_author_per_subreddit)
-# print(count_array.head(5))
 
 # normalize each row to have a sum of 1
 def normalize_row(df):
@@ -62,8 +62,6 @@ def get_most_unique_subreddit_from_counts(count_array):
     # Return index of first occurrence of maximum over requested axis
     return get_index_max_of_row(activity_centered_array)
 
-most_unique_sr = get_most_unique_subreddit_from_counts(count_array)
-
 def get_most_unique_subreddit_from_treatment(treatment_file_name):
     submissions = pd.read_csv(treatment_file_name)
     counts_author_subreddit_pair = count_by_author_and_subreddit(submissions)
@@ -71,12 +69,20 @@ def get_most_unique_subreddit_from_treatment(treatment_file_name):
     count_array = get_sparse_df(counts_author_per_subreddit)
     return get_most_unique_subreddit_from_counts(count_array)
 
+
+counts_author_subreddit_pair = count_by_author_and_subreddit(submissions)
+# print(counts_author_per_subreddit.head(5))
+counts_author_per_subreddit = get_author_per_subreddit(counts_author_subreddit_pair)
+count_array = get_sparse_df(counts_author_per_subreddit)
+# print(count_array.head(5))
+most_unique_sr = get_most_unique_subreddit_from_counts(count_array)
+
 most_unique_sr2 = get_most_unique_subreddit_from_treatment(TREATMENT_FILE_PATH)
 
 assert most_unique_sr.equals(most_unique_sr2)
 
 # output to csv
-# most_unique_sr.to_csv(OUTPUT_FILE_NAME)
+most_unique_sr.to_csv(OUTPUT_FILE_NAME)
 
 end = time.time()
 print('runtime = ' + str(end-start))
